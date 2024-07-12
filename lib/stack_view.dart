f04adcf6cad4ab659ab8acd6c72d2c75a16d53a2
Muhammad Ali Nizami <kustalics@gmail.com>
2024-07-12 15:59:46 +0500

    Bottom Bar Added

--- lib/stack_view.dart
+++ lib/stack_view.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:login_project/onboarding_view.dart';
 
-class StackWidgetDemo extends StatelessWidget {
-  const StackWidgetDemo({super.key});
+class StackWidgetDemo extends StatefulWidget {
+  StackWidgetDemo({super.key});
+
+  @override
+  State<StackWidgetDemo> createState() => _StackWidgetDemoState();
+}
+
+class _StackWidgetDemoState extends State<StackWidgetDemo> {
+  List<Widget> bodyWidgets = [
+    OnBoardingView(),
+    Center(
+      child: Text("Add"),
+    ),
+    Center(
+      child: Text("More"),
+    ),
+  ];
+
+  int selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -9,16 +27,16 @@ class StackWidgetDemo extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Stack Widget Demo"),
       ),
-      body: Stack(
-        children: [
-          Image.asset('assets/images/stack.png'),
-          const Positioned.fill(
-            child: Icon(
-              Icons.play_arrow,
-              size: 50,
-              color: Colors.white,
-            ),
-          )
+      body: bodyWidgets.elementAt(selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (val) {
+          selectedIndex = val;
+          setState(() {});
+        },
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.add), label: 'Add'),
+          BottomNavigationBarItem(icon: Icon(Icons.more_horiz), label: 'More'),
         ],
       ),
     );
